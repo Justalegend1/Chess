21db228bf5b37b2adcf8a868b1867d20a8624a7c
Никита Копылов <nekopylov@edu.hse.ru>
2023-01-07 05:59:24 +0500

    added board

---
+++ src/models/Cell.ts
@@ -0,0 +1,25 @@
+import { throws } from "assert";
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
+
+export class Cell{
+    readonly x : number;
+    readonly y : number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board:Board;
+    available: boolean // можно ли поставить сюда фигуру
+    id: number; // для ключей в реакте
+
+    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+        this.board = board;
+        this.available = false;
+        this.id = Math.random();
+    }
+
+}
